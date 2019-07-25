3fe46c11dd7526040fe9e231115e87189b5853c3
すー <s1732087AP@s.chibakoudai.jp>
2019-07-25 11:20:00 +0900

    Update:

--- js/mychart.js
+++ js/mychart.js
@@ -1,78 +1,41 @@
-// グラフ化するデータ系列のサンプル
-const sampleData = {
-  labels: ["p0", "p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8", "p9", "p10", "p11"],
-  sample1: [1.9, 2.32, 1.52, 0.79, 1.37, 1.28, 1.92, 1.44, 2.58, -0.01, 0.71, 4.25],
-  sample2: [7.01, -2.15, -7.29, 1.71, 0.72, -4.83, 2.75, 4.11, 3.08, -2.45, 3.05, -3.93],
-  sample3: [-2.97, -2.15, -0.3, -0.65, -8.84, 0.28, 2.95, 2.79, 1.79, 2.87, 0.16, 0.31],
-  timestamp: ["2018/04/16 22:18", "2018/04/16 23:18", "2018/04/17 00:18", "2018/04/17 01:18", "2018/04/17 02:18", "2018/04/17 09:18", "2018/04/17 10:18", "2018/04/17 11:18", "2018/04/17 12:18", "2018/04/17 13:18", "2018/04/17 14:18", "2018/04/17 15:18"]
-};
-
-// グラフ作成の手順を定義
-const loadCharts = function () {
-  const chartDataSet = {
-    type: 'line',
-    data: {
-      labels: sampleData.labels,
-      datasets: [{
-        label: 'sample1',
-        data: sampleData.sample1,
-        backgroundColor: 'rgba(60, 160, 220, 0.3)',
-        borderColor: 'rgba(60, 160, 220, 0.8)'
-      }, {
-        label: 'sample2',
-        data: sampleData.sample2,
-        backgroundColor: 'rgba(60, 190, 20, 0.3)',
-        borderColor: 'rgba(60, 190, 20, 0.8)'
-      }]
-    },
-    options: {}
-  };
-
-  const ctx = document.createElement('canvas');
-  document.getElementById('chart-area').appendChild(ctx);
-  new Chart(ctx, chartDataSet);
+const Data = {
+    labels: [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    data: [1, 1, 1, 1, 1, 1, 1, 1]
 };
 
 
-const Data={
-  labels:[-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10],
-  data:[1,1,1,1,1,1,1,1]
-};
-
-
-const loadCharts2 = function(data){
+const loadCharts2 = function (data) {
     console.log(data);
-  const chartDataSet = {
-    type: 'line',
-      data: {
-      labels: Data.labels,
-      datasets: [{
-        type: "line",
-        label: 'sample1',
-        data: [{x:1,y:30}],
-        backgroundColor: 'rgba(200, 100, 100, 1.0)',
-        pointRadius:10,
-        // borderColor: 'rgba(60, 160, 220, 0.8)'
-    },{
-        type: "line",
-        label: 'sample2',
-        data: data,
-        backgroundColor: 'rgba(0, 0, 0, 0.0)',
-        borderColor: 'rgba(60, 160, 220, 0.8)'
-          }
-      ]},
-      options: {
-        animation: {
-          duration: 0,
+    const chartDataSet = {
+        type: 'line',
+        data: {
+            labels: Data.labels,
+            datasets: [{
+                type: "line",
+                label: 'sample1',
+                data: [{x: 1, y: 30}],
+                backgroundColor: 'rgba(200, 100, 100, 1.0)',
+                pointRadius: 10,
+                // borderColor: 'rgba(60, 160, 220, 0.8)'
+            }, {
+                type: "line",
+                label: 'sample2',
+                data: data,
+                backgroundColor: 'rgba(0, 0, 0, 0.0)',
+                borderColor: 'rgba(60, 160, 220, 0.8)'
+            }
+            ]
+        },
+        options: {
+            animation: {
+                duration: 0,
+            }
         }
-      }
-  };
+    };
 
-  const ctx = document.createElement('canvas');
-  // document.getElementById('chart-area').appendChild(ctx);
-  // new Chart(ctx, chartDataSet);
-  var myChart = new  Chart( document.getElementById("mycanvas")
-      .getContext("2d"),chartDataSet);
+    const ctx = document.getElementById("mycanvas")
+        .getContext("2d");
+    const myChart = new Chart(ctx, chartDataSet);
 
 };
 
@@ -80,12 +43,12 @@ let data = new Array();
 let timer;
 let i = 0;
 
-const ManageLine = function() {
-  data.push((Data.labels[i] * Data.labels[i]) - (Data.labels[i] / 2) + 4);
-  loadCharts2(data);
-  if(i >= Data.labels.length)clearInterval(timer);
-  i++;
+const ManageLine = function () {
+    data.push((Data.labels[i] * Data.labels[i]) - (Data.labels[i] / 2) + 4);
+    loadCharts2(data);
+    if (i >= Data.labels.length) clearInterval(timer);
+    i++;
 };
-window.onload = function(){
-  timer = setInterval("ManageLine()", 500);
+window.onload = function () {
+    timer = setInterval("ManageLine()", 500);
 };
