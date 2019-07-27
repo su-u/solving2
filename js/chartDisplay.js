46ee46a506faf5dfee179eb0646aeb0aac92532b
すー <s1732087AP@s.chibakoudai.jp>
2019-07-27 13:45:57 +0900

    Update chartDisplay.js

---
+++ js/chartDisplay.js
@@ -0,0 +1,35 @@
+const ChartsDisplay = data => {
+    console.log(data);
+    const chartDataSet = {
+        type: 'line',
+        data: data,
+        options: {
+            animation: {
+                duration: 0,
+            }
+        }
+    };
+
+    const ctx = document.getElementById("mycanvas")
+        .getContext("2d");
+    ctx.canvas.parentNode.style.height = '90%';
+    ctx.canvas.parentNode.style.width = '70%';
+    const myChart = new Chart(ctx, chartDataSet);
+};
+
+// {
+//     labels: Data.labels,
+//         datasets: [{
+//     type: "line",
+//     label: 'sample1',
+//     data: [{x: 1, y: 30}],
+//     backgroundColor: 'rgba(200, 100, 100, 1.0)',
+//     pointRadius: 10,
+// }, {
+//     type: "line",
+//     label: 'sample2',
+//     data: data,
+//     backgroundColor: 'rgba(0, 0, 0, 0.0)',
+//     borderColor: 'rgba(60, 160, 220, 0.8)'
+// }]
+// }
