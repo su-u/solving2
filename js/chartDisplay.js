895af79769ac35a6de42fc7ae5b40ee38929d84e
すー <aaaiiiuuu35@gmail.com>
2019-07-29 23:43:46 +0900

    Update chartDisplay.js

--- js/chartDisplay.js
+++ js/chartDisplay.js
@@ -16,19 +16,3 @@ const ChartsDisplay = (data) => {
     const myChart = new Chart(ctx, chartDataSet);
 };
 
-// {
-//     labels: Data.labels,
-//         datasets: [{
-//     type: "line",
-//     label: 'sample1',
-//     data: [{x: 1, y: 30}],
-//     backgroundColor: 'rgba(200, 100, 100, 1.0)',
-//     pointRadius: 10,
-// }, {
-//     type: "line",
-//     label: 'sample2',
-//     data: data,
-//     backgroundColor: 'rgba(0, 0, 0, 0.0)',
-//     borderColor: 'rgba(60, 160, 220, 0.8)'
-// }]
-// }
