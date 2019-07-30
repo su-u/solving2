d1c39fd58c277b6c2e949597fde7c9fc121b2de5
すー <aaaiiiuuu35@gmail.com>
2019-07-30 16:25:31 +0900

    dot

--- js/calculation.js
+++ js/calculation.js
@@ -1,17 +1,19 @@
 const EPS = 0.0001; // constは定数の定義
 
 const nibun = (a, b, func) => {
-    let dot = [];
+    let centers = [];
+    let dotes = [];
     let count = 1;
     let c;
     do {
+        dotes.push([a,b]);
         c = (a + b) / 2.0; // 2分計算
         if (func(c) * func(a) < 0) b = c;
         // 式(1.2)
         else a = c; // 式(1.3)
-        dot.push(c);
+        dotes.push(c);
         count += 1;
     } while (Math.abs(a - b) > EPS); // 収束判別　式(1.4)の変形
-    return dot;
+    return [centers,dotes];
 };
 
--- js/mychart.js
+++ js/mychart.js
@@ -16,43 +16,54 @@ const func = x =>{
     return Math.pow(x, 3.0) + x - 1.0;
 };
 
-let dot = nibun(0.0,1.0,func);
+let [centers, dotes] = nibun(0.0,1.0,func);
 
 const labels = CreateLabels(-5,5);
 for (let j = 0; j < labels.length; j++) {
     data.push(func(labels[j]));
 }
-const chartData = (data) =>{
+const chartData = (data,center, dot1, dot2) =>{
+    const labe = CreateLabels(dot1 - 1,dot2 + 1);
+    console.log(labe);
     return  {
-        labels: CreateLabels(-5,5),
+        labels: labe,
         datasets: [{
             type: "line",
-            label: 'sample1',
-            data: [{x: 1, y: 30}],
+            label: 'dot',
+            data: [{x:dot1, y:0},{x:dot2,y:1}],
+            backgroundColor: 'rgb(57,166,79)',
+            pointRadius: 10,
+        },{
+            type: "line",
+            label: 'center',
+            data: [{x:center,y:-1}],
             backgroundColor: 'rgba(200, 100, 100, 1.0)',
             pointRadius: 10,
-        }, {
-            type: "line",
-            label: 'sample2',
-            data: data,
-            backgroundColor: 'rgba(0, 0, 0, 0.0)',
-            borderColor: 'rgba(60, 160, 220, 0.8)'
-    }]}
+        },
+        // {
+        //     type: "line",
+        //     label: 'line',
+        //     data: data,
+        //     backgroundColor: 'rgba(0, 0, 0, 0.0)',
+        //     borderColor: 'rgba(60, 160, 220, 0.8)'
+        // }
+    ]}
 };
 
 const ManageLine = function () {
-    ChartsDisplay(chartData(data));
+    ChartsDisplay(chartData(data,centers[i],dotes[i][0],dotes[i][1]));
     if (i >= labels.length) clearInterval(timer);
     i++;
 };
 
 const start = function () {
-    timer = setInterval("ManageLine()", 500);
+    timer = setInterval("ManageLine()", 1000);
 };
 window.onload = function () {
     ChartsDisplay(chartData);
 };
 
 console.log(data);
-console.log(dot);
+console.log(centers);
+console.log(dotes);
 console.log(labels);
