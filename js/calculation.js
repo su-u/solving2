d5cd856e0e584eb73d53436662f4ddbe10ba193a
すー <aaaiiiuuu35@gmail.com>
2019-08-04 00:08:54 +0900

    Update: indents

--- js/calculation.js
+++ js/calculation.js
@@ -3,17 +3,17 @@ const EPS = 0.0001; // constは定数の定義
 const nibun = (a, b, func) => {
     let centers = [];
     let dotes = [];
-let count = 1;
-let c;
-do {
-    dotes.push([a,b]);
-    c = (a + b) / 2.0; // 2分計算
-    if (func(c) * func(a) < 0) b = c;
-    // 式(1.2)
-    else a = c; // 式(1.3)
-    centers.push(c);
-    count += 1;
-} while (Math.abs(a - b) > EPS);
-return [centers,dotes];
+    let count = 1;
+    let c;
+    do {
+        dotes.push([a, b]);
+        c = (a + b) / 2.0; // 2分計算
+        if (func(c) * func(a) < 0) b = c;
+        // 式(1.2)
+        else a = c; // 式(1.3)
+        centers.push(c);
+        count += 1;
+    } while (Math.abs(a - b) > EPS);
+    return [centers, dotes];
 };
 
